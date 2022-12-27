1d1f30ee9959641617c36ad4f6040e95e7537a11
Abhishek <chhadios13@gmail.com>
2022-12-27 13:35:08 +0530

    redirect12

--- index.js
+++ index.js
@@ -34,13 +34,12 @@ app.use(cors());
 // express.js routes
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
-
+});
 app.use('/stocks', stockRoutes);
 app.use('/user', userRoutes);
 app.use('/purchased', purchasedStockRoutes);
 app.use('/logs', actionLogRoutes);
 app.use('/transactions', transactionRoutes);
-});
 app.get('*', (req, res) => {
   res.status(404).sendFile(__dirname + '/not_found.html');
 });
